6274d4987f51ff61278917f6152c0b394179307a
ahrarhayat <ahrarhayat@gmail.com>
2017-11-03 12:06:49 +0600

    Initial commit

---
+++ src/SimpleDotCom.java
@@ -0,0 +1,31 @@
+public class SimpleDotCom {
+    int [] LocationCells;
+    int NumOfHits;
+  public String Checkyourself(String UserGuess )
+   {
+       int guess = Integer.parseInt(UserGuess);
+       String result = "miss";
+       for(int cell:LocationCells)
+       {
+           if(guess==cell)
+           {
+               result = "hit";
+               NumOfHits++ ;
+               break;
+           }
+       }
+       if(NumOfHits==LocationCells.length)
+       {
+           result ="kill";
+
+       }
+       System.out.println(result);
+       return result;
+  }
+  public void  setLocationCells(int [] loc)
+    {
+        LocationCells = loc;
+    }
+
+}
+
